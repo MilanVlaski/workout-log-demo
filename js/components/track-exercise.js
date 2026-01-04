e29d9d31ceec46551d7021b30debefc1427264dc
Milan Vlaški <milan.vlaski321@gmail.com>
2026-01-04 16:12:10 +0100

    feat: render sets

--- js/components/track-exercise.js
+++ js/components/track-exercise.js
@@ -2,11 +2,20 @@
  * Depends on shoelace, and sl-input and sl-button being loaded.
  */
 class TrackExercise extends HTMLElement {
+
+    // Define the static constant using the class name later
+    static setInput = /*html*/`<sl-input class="reps" name="reps"></sl-input>`
+
     connectedCallback() {
-        const exerciseName = this.getAttribute('name') || 'Exercise';
+        const exerciseName = this.getAttribute('name') || 'Exercise'
+        
+        const dataAttr = this.getAttribute('data')
+        const data = JSON.parse(dataAttr)
+        
+        const repsHtml = this.repsHtml(data)
+
         this.innerHTML = /*html*/`
             <sl-card class="exercise-form">
-
                 <div slot="header">
                     <h1 contentEditable>${exerciseName}</h1>
                     <sl-icon-button name="x" label="Exit"></sl-icon-button>
@@ -14,15 +23,12 @@ class TrackExercise extends HTMLElement {
 
                 <form class="sets">
                     <div class="set">
-
                         <div class="repss">
-                            <sl-input class="reps" name="reps"></sl-input>
+                            ${repsHtml}
                             <sl-button class="reps" variant="primary">
                                 <sl-icon name="plus"></sl-icon>
                             </sl-button>
-
                         </div>
-
                         <sl-input placeholder="Weight" name="weight" class="weight"></sl-input>
                     </div>
 
@@ -37,15 +43,20 @@ class TrackExercise extends HTMLElement {
                             <sl-button type="submit" variant="primary">Finish</sl-button>
                         </div>
                     </div>
-
-                    </fieldset>
-
                 </form>
             </sl-card>
-            `
+            `;
     }
 
+    repsHtml(data) {
+        const input = TrackExercise.setInput;
 
+        if (!data || !data.sets) {
+            return input;
+        } else {
+            return Array(data.sets.length).fill(input).join('\n');
+        }
+    }
 }
 
 customElements.define('track-exercise', TrackExercise);
