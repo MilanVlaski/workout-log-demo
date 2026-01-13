4fdac7e8105a149dd59139a23a67c6394452f58e
Milan Vlaški <milan.vlaski321@gmail.com>
2026-01-13 15:07:54 +0100

    fix: style of header

--- js/components/header.js
+++ js/components/header.js
@@ -2,10 +2,10 @@ class Header extends HTMLElement {
     connectedCallback() {
         this.innerHTML = /*html*/`
         <header>
-        <nav>
-            <a href="edit-workout.html">Home</a>
-            <a href="workout-log.html">Log</a>
-        </nav>
+        <sl-button-group label="Navigation">
+            <sl-button href="edit-workout.html" pill>Start Workout</sl-button>
+            <sl-button href="workout-log.html" pill>Log</sl-button>
+        </sl-button-group>
         </header>
         `
     }
